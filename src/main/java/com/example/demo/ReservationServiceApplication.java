4c3b4b50a1ad20f0e101bdb89964c54b1a19661b
Asus <Asus@DESKTOP-7FSGV49>
2019-03-21 00:54:05 -0500

    Enable discovery

--- src/main/java/com/example/demo/ReservationServiceApplication.java
+++ src/main/java/com/example/demo/ReservationServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.lettuce.core.dynamic.annotation.Param;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 	
