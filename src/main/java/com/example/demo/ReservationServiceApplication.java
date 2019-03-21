8540d76fa90689a4d19bf6be09179a569f14c362
Asus <Asus@DESKTOP-7FSGV49>
2019-03-20 23:10:37 -0500

    Get configuration from git server

--- src/main/java/com/example/demo/ReservationServiceApplication.java
+++ src/main/java/com/example/demo/ReservationServiceApplication.java
@@ -8,13 +8,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.lettuce.core.dynamic.annotation.Param;
 
@@ -33,6 +37,19 @@ public class ReservationServiceApplication {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageController {
+	@Value("${message}")
+	private String message;
+	
+	@RequestMapping("/message")
+	String message() {
+		return this.message;
+	}
+	
 }
 
 //Data Access Layer
